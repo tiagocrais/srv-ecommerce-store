35636880f599aff9beb42da768614e1058d0edf2
Tiago Crais Camargo <tiagocrais@gmail.com>
2023-12-05 21:45:56 -0300

    Projeto criado

---
+++ src/main/java/br/com/tiagocrais/ecommerce/store/service/SrvEcommerceStoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagocrais.ecommerce.store.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvEcommerceStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvEcommerceStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tiagocrais/ecommerce/store/service/SrvEcommerceStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagocrais.ecommerce.store.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvPublicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
