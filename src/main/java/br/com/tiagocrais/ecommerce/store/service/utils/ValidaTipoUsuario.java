0c0c563f598fd373d3b5ad203796d2d9fb060a6c
Tiago Crais Camargo <tiagocrais@gmail.com>
2023-12-17 15:14:53 -0300

    Removendo imports não utilizados

--- src/main/java/br/com/tiagocrais/ecommerce/store/service/utils/ValidaTipoUsuario.java
+++ src/main/java/br/com/tiagocrais/ecommerce/store/service/utils/ValidaTipoUsuario.java
@@ -1,8 +1,5 @@
 package br.com.tiagocrais.ecommerce.store.service.utils;
 
-import br.com.tiagocrais.ecommerce.store.service.usecase.ClienteUseCase;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
